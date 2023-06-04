63ad09b686ed2bebf38f6ce8bdf655f75aa0256a
yeo sugyeong <ssyeo1204@gmail.com>
2023-06-04 19:04:10 +0900

    네번째 영역 html 수정

--- js/index.js
+++ js/index.js
@@ -24,24 +24,24 @@ new Swiper(".first-banner", {
 
 
 // 네 번째 영역 / 제품 탭
-const productTabs = document.querySelectorAll('.product-tab-area li')
-const productTabItems = document.querySelectorAll('.fourth-area-inner .product-item')
-let activeCon = '';
+// const productTabs = document.querySelectorAll('.product-tab-area li')
+// const productTabItems = document.querySelectorAll('.fourth-area-inner .product-item')
+// let activeCon = '';
 
-for(let i = 0; i < productTabs.length; i++) {
-  productTabs[i].querySelector('.tab').addEventListener('click', function(e) {
-    e.preventDefault();
+// for(let i = 0; i < productTabs.length; i++) {
+//   productTabs[i].querySelector('.tab').addEventListener('click', function(e) {
+//     e.preventDefault();
 
-    for(let j=0; j < productTabs.length; j++) {
-      productTabs[j].classList.remove('active')
-      productTabItems[j].style.display = 'none'
-    }
-    this.parentNode.classList.add('active')
+//     for(let j=0; j < productTabs.length; j++) {
+//       productTabs[j].classList.remove('active')
+//       productTabItems[j].style.display = 'none'
+//     }
+//     this.parentNode.classList.add('active')
 
-    activeCon = this.getAttribute('href');
-    document.querySelector(activeCon).style.display = 'flex';
-  })
-}
+//     activeCon = this.getAttribute('href');
+//     document.querySelector(activeCon).style.display = 'flex';
+//   })
+// }
 
 
 
